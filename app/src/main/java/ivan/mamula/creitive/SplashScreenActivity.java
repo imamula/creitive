fc63c3f77a1c3791a87a890b84518177b0bee221
Ivan Mamula <mamula82@gmail.com>
2018-03-09 15:26:40 +0100

    Splash screen added.

--- app/src/main/java/ivan/mamula/creitive/SplashScreenActivity.java
+++ app/src/main/java/ivan/mamula/creitive/SplashScreenActivity.java
@@ -3,11 +3,12 @@ package ivan.mamula.creitive;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
     }
 }
